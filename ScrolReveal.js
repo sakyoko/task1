e083154a5aa5f63d0561c2d020167f5b10e28094
sakyoko <66012134+sakyoko@users.noreply.github.com>
2020-12-10 19:48:03 +0900

    Create ScrolReveal.js

---
+++ ScrolReveal.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.photo', {
+    delay: 200,
+    distance: '50px', 
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.akita img', {
+    delay: 800,
+    distance: '50px', 
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.right-contents img', {
+    delay: 900,
+    distance: '50px', 
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.hobby-photo', {
+    delay: 300,
+    distance: '50px', 
+    origin: 'left'
+});
